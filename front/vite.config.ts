d1b12ca6c2d7cbd4ebda9211385bc04081a51e44
Viktoriya Tolstaia <145439367+Vviktoriyya@users.noreply.github.com>
2026-02-05 21:39:03 +0200

    feat: додала футер та налаштувала деплой

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: './',
+  base: '/music/',
   server: {
     proxy: {
       '/api': 'http://localhost:5000',
