678f694df5bf8ac4f3c6b530eacb0e4adf403922
Viktoriia Tolstaia <tolstaavika10@gmail.com>
2026-07-29 17:30:31 +0300

    fix album api production url

--- front/src/pages/album/services/albumService.ts
+++ front/src/pages/album/services/albumService.ts
@@ -1,10 +1,14 @@
 import axios from "axios";
-import type { AlbumData } from "../interfaces/AlbumInfo.ts";
+import type {AlbumData} from "../interfaces/AlbumInfo.ts";
+
+const API_URL = import.meta.env.VITE_API_URL;
+
 export const fetchAlbumById = async (id: string): Promise<AlbumData> => {
-    const res = await axios.get(`/api/album/${id}`);
+    const res = await axios.get(`${API_URL}/api/album/${id}`);
     return res.data;
 };
+
 export const fetchAlbum = async (): Promise<AlbumData> => {
-    const res = await axios.get(`/api/album/random`);
+    const res = await axios.get(`${API_URL}/api/album/random`);
     return res.data;
 };
